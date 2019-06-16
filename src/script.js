2a46272177b196ca38cedd926ba2753ac15c61c5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-16 19:38:31 -0400

    Add iterative generation of training data

--- src/script.js
+++ src/script.js
@@ -2,18 +2,23 @@ canvas = document.querySelector('#canvas');
 ctx = canvas.getContext('2d');
 
 inputs = [];
-r = Math.random() * 10;
-for (var i = 0; i < r; i ++) {
-	x = Math.random() * canvas.width;
-	y = Math.random() * canvas.height;
+for (var i = 0; i < 10; i ++) {
+	ctx.fillStyle = 'white';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	r = Math.random() * 10;
+	for (var j = 0; j < r; j ++) {
+		x = Math.random() * canvas.width;
+		y = Math.random() * canvas.height;
 
-	ctx.beginPath();
-	ctx.arc(x, y, 25, 0, 2 * Math.PI, false);
-	ctx.fillStyle = 'black';
-	ctx.fill();
-	
+		ctx.beginPath();
+		ctx.arc(x, y, 25, 0, 2 * Math.PI, false);
+		ctx.fillStyle = 'black';
+		ctx.fill();
+	}
 	// do this (resize) as soon as possible
 	inputs.push(tf.browser.fromPixels(canvas).resizeBilinear([50, 50]))
+	console.log(r);
 }
-console.log(r);
 inputs = tf.stack(inputs);
+
+// ctx.getImageData(0, 0, canvas.width, canvas.height)
