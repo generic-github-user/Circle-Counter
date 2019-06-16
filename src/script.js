2b05def0484a5135718756987e1fb2acde118a39
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-16 19:15:05 -0400

    Add random circle generation

--- src/script.js
+++ src/script.js
@@ -1,2 +1,14 @@
 canvas = document.querySelector('#canvas');
 ctx = canvas.getContext('2d');
+
+r = Math.random() * 10;
+for (var i = 0; i < r; i ++) {
+	x = Math.random() * canvas.width;
+	y = Math.random() * canvas.height;
+
+	context.beginPath();
+	context.arc(x, y, 25, 0, 2 * Math.PI, false);
+	context.fillStyle = 'black';
+	context.fill();
+}
+console.log(r);
