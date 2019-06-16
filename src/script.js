c63436c9282d70e1e1c527688a355eaf2157ea70
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-16 19:35:36 -0400

    Collect inputs, resize, and stack into an input tensor for training

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 canvas = document.querySelector('#canvas');
 ctx = canvas.getContext('2d');
 
+inputs = [];
 r = Math.random() * 10;
 for (var i = 0; i < r; i ++) {
 	x = Math.random() * canvas.width;
@@ -10,5 +11,9 @@ for (var i = 0; i < r; i ++) {
 	ctx.arc(x, y, 25, 0, 2 * Math.PI, false);
 	ctx.fillStyle = 'black';
 	ctx.fill();
+	
+	// do this (resize) as soon as possible
+	inputs.push(tf.browser.fromPixels(canvas).resizeBilinear([50, 50]))
 }
 console.log(r);
+inputs = tf.stack(inputs);
